c76c10440832f8ce7e027ef4bae1e521d51c263b
Rihab Chouikh <chouikh.rihab@gmail.com>
2021-05-16 14:45:40 +0200

    Header Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './Components/Navbar.js';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./Components/Navbar.js";
+import Header from "./Components/header.js";
 
 function App() {
   return (
+    <>
+   
     <Navbar/>
+    <Header/>
+    </>
+
   );
 }
 
---
+++ src/Components/header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Typed from "react-typed";
+
+
+const header = () => {
+    return (
+    <div classname="header-wrapper">
+        <div className="main-info">  
+
+         <h1>Web Developpement and WebSite promotions</h1>
+         <Typed classname="typed-text"
+         strings={["Web Design","Web Developpement","Facebook ADS SMM","google ADS"]}
+         typeSpeed={40}
+         backSpeed={60}
+        loop
+         />
+        
+        <button type="button" class="btn btn-outline-danger">Contact Me</button>
+        
+        </div>
+       
+
+    </div>
+
+
+    )
+}
+
+export default header
